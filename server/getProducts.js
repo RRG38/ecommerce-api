6d911091b87219cb4c8fadb8d7aa6c41271d07e1
Ryan Garren <rryangarren@gmail.com>
2022-01-11 20:43:58 -0700

    step 6 completed. server fully functional.

--- server/getProducts.js
+++ server/getProducts.js
@@ -1,7 +1,15 @@
 const products = require("../products.json");
 
 const getProducts = (req, res) => {
-  res.status(200).send(products);
+  const price = req.query?.price;
+
+  if (price) {
+    const filteredList = products.filter((product) => product.price >= price);
+    return res.status(200).send(filteredList);
+  } else {
+    console.log(`Price not found`);
+    res.status(200).send(products);
+  }
 };
 
 module.exports = getProducts;
