116599f766a39236b6fd69399e8d4a4aa29ee045
Ryan Garren <rryangarren@gmail.com>
2022-01-11 19:40:21 -0700

    completed step 3

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const products = require("../products.json");
 
 const app = express();
+
+app.get("/api/products", (req, res) => {
+  res.status(200).send(products);
+});
 
 const port = 3001;
 
